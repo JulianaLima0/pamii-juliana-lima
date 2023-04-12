3ad96b4f2acb3af3e13553ab14d1195a9f379cec
JulianaLima0 <S2csbin@gmail.com>
2023-04-12 10:45:07 -0300

    exercício feito: seis operações matemáticaso

---
+++ aula02/exercício/operacoes_matematicas.js
@@ -0,0 +1,56 @@
+// SOMA
+
+function soma (a, b) {
+    return a + b
+}
+
+console.log("O resultado da soma é:",soma(2, 23))
+
+
+
+// SUBTRAÇÃO
+
+function subtracao (a, b) {
+    return a - b
+}
+
+console.log("O resultado da subtração é: ",subtracao(16, 8))
+
+
+
+// DIVISÃO
+
+function divisao (a, b) {
+    return a / b
+}
+
+console.log("O resultado da divisão é:",divisao(1258, 4))
+
+
+
+// MULTIPLICAÇÃO
+
+function multiplicacao (a, b) {
+    return a * b
+}
+
+console.log("O resultado da multiplicação é:",multiplicacao(12, 8))
+
+
+// RADICIAÇÃO
+
+function raiz (a) {
+    return Math.sqrt(a) 
+}
+
+console.log("A raiz é:",raiz(25))
+
+
+
+// POTENCIAÇÃO
+
+function potencia (a, b) {
+    return a ** b
+}
+
+console.log("A potência é:",potencia(6, 2))
